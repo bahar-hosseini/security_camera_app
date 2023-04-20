4a8f4f1766cb98ff0f1cdf627fb7f0a67e01f571
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-04-20 02:04:25 -0400

    seed some data for user schema and use bcrypt to hash password

---
+++ backend/data/users.js
@@ -0,0 +1,29 @@
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const users = [
+  {
+    name: 'Yana',
+    email: 'yana@example.com',
+    password: bcrypt.hashSync('qazwsx', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Bahar',
+    email: 'bahar@example.com',
+    password: bcrypt.hashSync('qazwsx', 10),
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('qazwsx', 10),
+  },
+];
+
+export default users;
