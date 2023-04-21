166f14e8ed3555d3d79b164d4e9fa112bb3e30d0
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-04-20 22:13:15 -0400

    create schema for videos

---
+++ backend/models/videosModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+const videoSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    room: {
+     type:Number,
+     required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const Video = mongoose.model("Video", videoSchema);
+
+export default Video;
