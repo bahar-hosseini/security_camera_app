a1fe3fee5b4c38ec02ca60dc02b04865d9df0e37
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-04-21 18:10:22 -0400

    use liveVideos API to get all live videos

--- frontend/src/screen/VideosScreen.js
+++ frontend/src/screen/VideosScreen.js
@@ -1,76 +1,38 @@
 //External Modules
 import ReactPlayer from "react-player/lazy";
 import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import useFetch from "../useFetch";
 
 const VideosScreen = () => {
+
+  const {data : videos, isPending, error} = useFetch('/api/videos')
+
+  console.log(videos);
+
   return (
     <Container >
         <Col>
-      <Row className="d-flex d-flex align-items-center justify-content-center">
+        <Row className="d-flex d-flex align-items-center justify-content-center">
+        {videos.map(video => {
+            return (<Card
+            className="my-2 py-3"
+            style={{ width: "33vw", height: "auto" }}
+            key ={video._id}
+          >
+            <ReactPlayer
+              url={video.videoUrl}
+              controls={true}
+              width="100%"
+              height="100%"
+            />
 
-            <Card
-              className="my-2 py-3"
-              style={{ width: "33vw", height: "auto" }}
-            >
-              <ReactPlayer
-                url="https://videos-bolt-and-dash.s3.amazonaws.com/camera_01.mp4"
-                controls={true}
-                width="100%"
-                height="100%"
-              />
+            <Button variant="custom">
+              Room {video.room}
+            </Button>
+          </Card>
+            )
+        })}
 
-              <Button variant="custom">
-                Room 1
-              </Button>
-            </Card>
-
-            <Card
-              className="m-2 py-3"
-              style={{ width: "33vw", height: "auto" }}
-            >
-              <ReactPlayer
-                url="https://videos-bolt-and-dash.s3.us-east-2.amazonaws.com/camera2_01.mp4"
-                controls={true}
-                width="100%"
-                height="100%"
-              />
-
-              <Button variant="custom" >
-                Room 2
-              </Button>
-            </Card>
-
-            <Card
-              className="m-2 py-3"
-              style={{ width: "33vw", height: "auto" }}
-            >
-              <ReactPlayer
-                url="https://videos-bolt-and-dash.s3.us-east-2.amazonaws.com/camera3_01.mp4"
-                controls={true}
-                width="100%"
-                height="100%"
-              />
-
-              <Button variant="custom" >
-                Room 3
-              </Button>
-            </Card>
-
-            <Card
-              className="m-2 py-3"
-              style={{ width: "33vw", height: "auto" }}
-            >
-              <ReactPlayer
-                url="https://videos-bolt-and-dash.s3.us-east-2.amazonaws.com/camera4_01.mp4"
-                controls={true}
-                width="100%"
-                height="100%"
-              />
-              <Button variant="custom" >
-                Room 4
-              </Button>
-            </Card>
-          {/* </Container> */}
       </Row>
         </Col>
     </Container>
