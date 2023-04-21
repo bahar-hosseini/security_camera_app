edf25a85dfa08c9afc954e614e38d1948fce0add
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-04-20 20:55:22 -0400

    Add Col and Row for video items

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 //External Modules
 import React from "react";
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 import { useLoginContext } from "../providers/LoginProvider";
@@ -17,20 +17,16 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto ">
-            <NavDropdown>
-              {isLoggedIn && (
-                <>
-                  <LinkContainer to="/videos">
-                    <NavDropdown.Item>Videos</NavDropdown.Item>
-                  </LinkContainer>
+            {isLoggedIn && (
+              <>
+                <LinkContainer to="/videos">
+                  <Nav.Link>Videos</Nav.Link>
+                </LinkContainer>
 
-                  <NavDropdown.Item onClick={() => logout()}>
-                    Logout
-                  </NavDropdown.Item>
-                </>
-              )}
-              {!isLoggedIn && <NavDropdown.Item>Sign In</NavDropdown.Item>}
-            </NavDropdown>
+                <Nav.Link onClick={() => logout()}>Logout</Nav.Link>
+              </>
+            )}
+            {!isLoggedIn && <Nav.Link>Sign In</Nav.Link>}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- frontend/src/screen/VideosScreen.js
+++ frontend/src/screen/VideosScreen.js
@@ -4,63 +4,86 @@ import { Button, Card, Container, Row, Col } from "react-bootstrap";
 
 const VideosScreen = () => {
   return (
-    <Container>
-      <Col>
-        <Row className="d-flex align-items-center justify-content-center">
-          {/* <Container > */}
-          <Card className="m-2 py-3" style={{ width: "33vw", height: "auto" }}>
-            <ReactPlayer
-              url="videos/camera1/camera_01.mp4"
-              controls={true}
-              width="100%"
-              height="100%"
-            />
+    <Container className="d-flex  align-items-center justify-content-center">
+      <Row>
+        <Col>
+          <Container>
+            <Card
+              className="my-2 py-3"
+              style={{ width: "33vw", height: "45vh" }}
+            >
+              <ReactPlayer
+                url="videos/camera1/camera_01.mp4"
+                controls={true}
+                width="100%"
+                height="100%"
+              />
 
-            <Button variant="warning" size="md">
-              Room 1
-            </Button>
-          </Card>
-          {/* </Container> */}
-          {/* <Container > */}
-          <Card className="m-2 py-3" style={{ width: "33vw", height: "auto" }}>
-            <ReactPlayer
-              url="videos/camera2/camera2_01.mov"
-              controls={true}
-              width="100%"
-              height="100%"
-            />
+              <Button variant="warning" size="md">
+                Room 1
+              </Button>
+            </Card>
+          </Container>
+        </Col>
+        <Col>
+          <Container>
+            <Card
+              className="m-2 py-3"
+              style={{ width: "33vw", height: "45vh" }}
+            >
+              <ReactPlayer
+                url="videos/camera2/camera2_01.mov"
+                controls={true}
+                width="100%"
+                height="100%"
+              />
 
-            <Button variant="warning" size="md">
-              Room 2
-            </Button>
-          </Card>
-          {/* </Container> */}
-          <Card className="m-2 py-3" style={{ width: "33vw", height: "auto" }}>
-            <ReactPlayer
-              url="videos/camera3/camera3_01.mp4"
-              controls={true}
-              width="100%"
-              height="100%"
-            />
+              <Button variant="warning" size="md">
+                Room 2
+              </Button>
+            </Card>
+          </Container>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Container>
+            <Card
+              className="m-2 py-3"
+              style={{ width: "33vw", height: "45vh" }}
+            >
+              <ReactPlayer
+                url="videos/camera3/camera3_01.mp4"
+                controls={true}
+                width="100%"
+                height="100%"
+              />
 
-            <Button variant="warning" size="md">
-              Room 3
-            </Button>
-          </Card>
-          <Card className="m-2 py-3" style={{ width: "33vw", height: "auto" }}>
-            <ReactPlayer
-              url="videos/camera4/camera4_01.mp4"
-              controls={true}
-              width="100%"
-              height="100%"
-            />
-
-            <Button variant="warning" size="md">
-              Room 4
-            </Button>
-          </Card>
-        </Row>
-      </Col>
+              <Button variant="warning" size="md">
+                Room 3
+              </Button>
+            </Card>
+          </Container>
+        </Col>
+        <Col>
+          <Container>
+            <Card
+              className="m-2 py-3"
+              style={{ width: "33vw", height: "45vh" }}
+            >
+              <ReactPlayer
+                url="videos/camera4/camera4_01.mp4"
+                controls={true}
+                width="100%"
+                height="100%"
+              />
+              <Button variant="warning" size="md">
+                Room 4
+              </Button>
+            </Card>
+          </Container>
+        </Col>
+      </Row>
     </Container>
   );
 };
